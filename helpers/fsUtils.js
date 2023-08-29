aa78066d4379e11cee2f1a574e185266c6a2c2b5
Samimi <smokhadar@gmail.com>
2023-08-29 16:23:14 -0700

    added delete route

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,10 +1,7 @@
 const fs = require('fs');
-const util = require('util');
-
-const readFromFile = util.promisify(fs.readFile);
 
 const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+  fs.writeFileSync(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 
@@ -20,4 +17,4 @@ const readAndAppend = (content, file) => {
   });
 };
 
-module.exports = { writeToFile, readAndAppend, readFromFile };
+module.exports = { readAndAppend, writeToFile };
--- routes/notes.js
+++ routes/notes.js
@@ -1,29 +1,30 @@
-// const path = require('path');
 const notes = require('express').Router();
 const uuid = require('../helpers/uuid');
-const { readAndAppend } = require('../helpers/fsUtils');
+const { readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const notesFiles = require('../db/db.json');
 
 notes.get('/', (req, res) => {
     res.json(notesFiles);
 })
 
+notes.delete('/:id', (req, res) => {
+    console.log(req.params.id);
+    const filteredNotes = notesFiles.filter((n) => n.id !== req.params.id);
+    writeToFile( './db/db.json', filteredNotes);
+})
+
 notes.post('/', (req, res) => {
     console.info(`${req.method} request received.`)
-
     const { title, text } = req.body;
-
     if (title && text) {
         const newNote = {
             title,
             text,
-            note_id: uuid(),
+            id: uuid(),
         };
 
         readAndAppend(newNote, `./db/db.json`, (err) =>
-            err
-            ? console.error(err)
-            : console.log(
+            err? console.error(err) : console.log(
                 `Review for ${newNote.title} has been appended to JSON file`
             )
         );
